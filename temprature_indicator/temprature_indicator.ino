96557ab90a1588a34490759547da682faa44179f
Robby Decosemaeker <robby@MacBook-Pro-van-Robby-Decosemaeker.local>
2013-10-01 22:31:45 +0200

    Made the temperatur indecator more robust and added input abilities

--- temprature_indicator/temprature_indicator.ino
+++ temprature_indicator/temprature_indicator.ino
@@ -3,8 +3,7 @@ const int led2Pin = 5;
 const int led3Pin = 6;
 
 const int sensorPin = A0;
-
-const float baseLineTemp = 21.0;
+float baseLineTemp = 16.0;
 const int tempVariation = 2;
 
 void setup() {
@@ -18,20 +17,14 @@ void setup() {
   digitalWrite(led3Pin, LOW);
 }
 
+int previousValue = 0;
 void loop() {
   int sensorValue = analogRead(sensorPin);
-  Serial.print("Sensor Value: ");
-  Serial.print(sensorValue);
+  printTemperature(sensorValue);
+  sensorValue = (sensorValue + previousValue)/2;
+  printTemperature(sensorValue);
   
-  float voltage = (sensorValue/1024.0)*5.0;
-  Serial.print(", Voltage: ");
-  Serial.print(voltage);
-  
-  float temperature = ((voltage - .5) * 100);
-  Serial.print(", Temperature: ");
-  Serial.print(temperature);
-  Serial.print("\n");
-  
+  float temperature = calculateTemperature(sensorValue);
   //Too hot
   if(temperature > baseLineTemp + tempVariation){
     digitalWrite(led1Pin, LOW);
@@ -49,7 +42,29 @@ void loop() {
       digitalWrite(led2Pin, HIGH);
       digitalWrite(led3Pin, LOW);
     }
+    previousValue = sensorValue;
   }
-  
+  //read new baseline from serial
+  Serial.println("Set new baseline temp");
+  if (Serial.available() > 0) {
+    baseLineTemp = Serial.parseFloat();
+    Serial.println(baseLineTemp);
+  }
   delay(1000);
+}
+
+void printTemperature(int sensorValue){
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorValue);
+  
+  Serial.print(", Voltage: ");
+  Serial.print((sensorValue/1024.0)*5.0);
+  
+  Serial.print(", Temperature: ");
+  Serial.print(calculateTemperature(sensorValue));
+  Serial.print("\n");
+}
+
+float calculateTemperature(int sensorValue){
+  return ((((sensorValue/1024.0)*5.0) - .5) * 100);
 }
